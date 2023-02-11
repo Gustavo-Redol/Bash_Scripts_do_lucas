4f08c59f08258e6beefc6d4a06e957e94ff4868a
Lucas Turossi <lucasturossi@protonmail.com>
2023-02-11 12:38:31 -0300

    Update Script simples em escala.sh

--- Script simples em escala.sh
+++ Script simples em escala.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 comando=""
-for x  in n $(cat arquivo_com_lista_de_servidores_para_acesso);
+for x  in $(cat arquivo_com_lista_de_servidores_para_acesso);
 do
     comando=`ssh $x 'comando_executado'`
     echo "$x" " " "$comando" >> saída_do_comando.txt
