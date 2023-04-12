77452b46b6cb839a52ecba61b5572061029b8f0f
Lucas Turossi <lucasturossi@protonmail.com>
2023-04-12 10:07:37 -0300

    Add files via upload

--- Log_Analyzer.sh
+++ Log_Analyzer.sh
@@ -11,7 +11,9 @@ sed -i "s|$dir_path/||g; s|/$||" domains.txt
 
 for dir in $(cat domains.txt); do
     # Executa o comando e redireciona a saída para um arquivo de texto
-    cat $dir/logs/access_ssl_log | cut -d ' ' -f1 | sort | uniq -c | sort -unr | awk '{ print $1 "," $2 }' > /var/www/vhosts/logs_domains/$dir.csv
+    cat $dir/logs/access_ssl_log | cut -d ' ' -f1 | sort | uniq -c | sort -unr | awk '{ print $1 "," $2 }' > /var/www/vhosts/"$dir"/qtdipac_ssl_"$(date +%Y%m%d_%H%M%S)"_"$dir".csv
+    # Pega os 5 primeiros resultados da segunda coluna do arquivo e salva em mostips.txt
+    cat /var/www/vhosts/"$dir"/qtdipac_ssl_"$(date +%Y%m%d_%H%M%S)"_"$dir".csv | awk -F',' '{print $2}' | head -n 5 > /var/www/vhosts/"$dir"/mostips.txt
 done
 
 # Remove o arquivo txt com a lista de diretórios
