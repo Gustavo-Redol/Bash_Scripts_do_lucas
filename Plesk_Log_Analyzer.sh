ccf3121ba4091a975a5279b170acb4700ade5a3a
Lucas Turossi <lucasturossi@protonmail.com>
2023-04-12 17:05:36 -0300

    Update Plesk_Log_Analyzer.sh

--- Plesk_Log_Analyzer.sh
+++ Plesk_Log_Analyzer.sh
@@ -11,14 +11,20 @@ sed -i "s|$dir_path/||g; s|/$||" domains.txt
 
 for dir in $(cat domains.txt); do
     # Executa o comando e redireciona a saída para um arquivo de texto
-    cat $dir/logs/access_ssl_log | cut -d ' ' -f1 | sort | uniq -c | sort -unr | awk '{ print $1 "," $2 }' > /var/www/vhosts/"$dir"/qtdipac_ssl_"$(date +%Y%m%d_%H%M%S)"_"$dir".csv
+    cat "$dir"/logs/access_ssl_log | cut -d ' ' -f1 | sort | uniq -c | sort -rn | awk '{ print $1 "," $2 }' > /var/www/vhosts/"$dir"/qtdipac_ssl_"$(date +%Y%m%d_%H%M%S)"_"$dir".csv
     # Pega os 5 primeiros resultados da segunda coluna do arquivo e salva em mostips.txt
     cat /var/www/vhosts/"$dir"/qtdipac_ssl_"$(date +%Y%m%d_%H%M%S)"_"$dir".csv | awk -F',' '{print $2}' | head -n 5 > /var/www/vhosts/"$dir"/mostips.txt
 
     # Adiciona a linha abaixo para utilizar a variável $dir dentro do segundo loop
     dir_path="/var/www/vhosts/$dir"
-    for ip in $(cat $dir_path/mostips.txt); do
-        cat $dir_path/logs/access_ssl_log | grep "$ip" | cut -d '"' -f2 | uniq -c | sort -unr > $dir_path/most_accss_"$ip"_"$(date +%Y%m%d_%H%M%S)"_"$dir".csv
+    echo "Qtd, iP, Qtd, Mtd, Path" > "$dir_path"/Qtdipac_Final_Report.csv
+    for file in "$dir_path"/qtdipac_ssl_*; do
+        awk -F, -v OFS=',' '{print $1 "," $2 ",," $3 "," $4}' "$file" | awk -F, -v OFS=',' 'NR>1{$3=$3 OFS $4; $4=$5; $5=""; print}' >> "$dir_path"/Qtdipac_Final_Report.csv
+    done
+
+    echo "Qtd, iP, Qtd, Path" > "$dir_path"/Most_Accessed_Report.csv
+    for file in "$dir_path"/most_accss_*; do
+        awk -F, -v OFS=',' 'NR>1{$3=$1; $4=$2; $1=$2=""; print}' "$file" >> "$dir_path"/Most_Accessed_Report.csv
     done
 done
 
